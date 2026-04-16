d20920cc98bc2a0b68512df6bc2354b2f502f416
Liya2199 <125241370+Liya2199@users.noreply.github.com>
2026-04-16 09:57:59 +0800

    fix the mini-diff source, make it be git instead of none, use <leader>ghd to open diff

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -4,7 +4,13 @@ return {
     local diff = require("mini.diff")
     diff.setup({
       -- Disabled by default
-      source = diff.gen_source.none(),
+      --source = diff.gen_source.none(),
+      --source = diff.gen_source.save(), --对比上次保存
+      source = diff.gen_source.git(),-- 用 Git index 当参考（这就是默认行为）
+      view = {
+        style = 'sign',
+        signs = { add = '▎', change = '▎', delete = '' },
+      },
     })
   end,
 }
